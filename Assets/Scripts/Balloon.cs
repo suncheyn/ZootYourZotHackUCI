ce075be1f711ac2b8dea0f16a8dd9a09d8cf1a0f
Chey <32723997+suncheyn@users.noreply.github.com>
2019-02-16 13:58:10 -0800

    Revert "Revert "Merge branch 'master' of https://github.com/suncheyn/ZootYourZotHackUCI""
    This reverts commit cb827bd4aff07c1fe04deaaf76076cba610fabca.

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    // rigidbody.velocity = transform.forward;
+    // Vector2 pos = Vector2.zero;
+    // input = "balloon";
+    // float speed;
+    // float height;
+    // public Balloon balloon;
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     height = transform.localScale.y;
+    //     speed = 5f;
+    // }
+
+    // //transform.name = input;
+    //// transform.position =  pos;
+    //transform.name = input
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    float move = Input.GetAxis(input) * Time.deltaTime;
+    //}
+    public static int movespeed = 10;
+    public Vector3 userDirection = Vector3.up;
+    //public static Start()
+    //{
+    //}
+    public void Update()
+    {
+        transform.position += Vector3.up * Time.deltaTime;
+
+        transform.position += Vector3.right * Time.deltaTime;
+
+        transform.position += Vector3.left * Time.deltaTime;
+
+    }
+}
